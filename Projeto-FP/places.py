5ec626294b0e52a4ed25c84d521ea746cf043b9e
sle3pyy <simaomarquesantos@gmail.com>
2023-12-12 12:04:20 +0000

    er

--- Projeto-FP/places.py
+++ Projeto-FP/places.py
@@ -3,7 +3,7 @@ import requests
 def link(coordenadas,raiom,atraçõeslista):
     url="https://api.geoapify.com/v2/places"
     apiKey="&apiKey=048fde22cdb44f41963fb00652ca6298"
-    url1=url +"?categories=" + atraçõeslista +"&filter=circle:"+coordenadas[0]+","+coordenadas[1]+","+raiom+ "&bias=proximity:" + coordenadas[0]+","+coordenadas[1]+ "&limit=10"+apiKey
+    url1=url +"?categories=" + atraçõeslista +"&filter=circle:"+coordenadas[0]+","+coordenadas[1]+","+raiom+ "&bias=proximity:" + coordenadas[0]+","+coordenadas[1]+ "&limit=40"+apiKey
     print(url1)
     return url1
     #Função que cria e retorna o url da API
@@ -23,25 +23,29 @@ def main():
     raiom=str(raio*1000)
     distance=0
     #limit=input("Insira qual o número máximo de lugares que quer ver: ")
-    limit = 10
+    limit = 40
     #atrações=input("Insira as suas atrações desejadas separados por virgula, sem espaços: ")
-    atrações = "education,accommodation.hotel,accommodation"
+    atrações = "education,sport,accommodation"
     atraçõesList=atrações.split(",")
     apil=link(coordenadas,raiom,atrações)
     APIdata=request(apil)
 
-    #with open(r"C:\Users\Utilizador\Desktop\projeto-FP\Projeto-FP\categories.txt") as file:
-        #places = [file.readline()[:-1] for line in file]
+    with open(r"categories.txt") as file:
+        places = [file.readline()[:-1] for line in file]
     placesk = {}
     for i in range(limit):
         for j in range(len(atraçõesList)):
             if atraçõesList[j] in APIdata["features"][i]["properties"]["categories"]:
-                placesk.setdefault(atraçõesList[j], [])
-                placesk[atraçõesList[j]].append(APIdata["features"][i]["properties"]["name"])
-                distance = distance + APIdata["features"][i]["properties"]["distance"]
+                try:
+                    placesk.setdefault(atraçõesList[j], [])
+                    placesk[atraçõesList[j]].append(APIdata["features"][i]["properties"]["name"])
+                    distance = distance + APIdata["features"][i]["properties"]["distance"]
+                except:
+                    print("hi")
     medium_distance = distance / limit            
     print(placesk)
-    print("Distancia média: ",medium_distance/1000,"kms")
+    print("Distancia média:",medium_distance/1000,"kms")
+    print("número de lugares encontrados:", len(APIdata["features"]))
     #placesk é um dicionário com as categorias como chave
 
 main()
