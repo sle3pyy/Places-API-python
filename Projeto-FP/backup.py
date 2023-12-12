9b6fd489b91f908cce71cbc583a94acbd260f301
sle3pyy <simaomarquesantos@gmail.com>
2023-12-12 14:44:26 +0000

    erm

--- Projeto-FP/backup.py
+++ Projeto-FP/backup.py
@@ -1,9 +1,10 @@
 import requests
 
-def link(coordenadas,raiom,atraçõeslista):
+def link(coordenadas,raiom,atraçõeslista,limit):
     url="https://api.geoapify.com/v2/places"
     apiKey="&apiKey=048fde22cdb44f41963fb00652ca6298"
-    url1=url +"?categories=" + atraçõeslista +"&filter=circle:"+coordenadas[0]+","+coordenadas[1]+","+raiom+ "&bias=proximity:" + coordenadas[0]+","+coordenadas[1]+ "&limit=10"+apiKey
+    limit = str(limit)
+    url1=url +"?categories=" + atraçõeslista +"&filter=circle:"+coordenadas[0]+","+coordenadas[1]+","+raiom+ "&bias=proximity:" + coordenadas[0]+","+coordenadas[1]+ "&limit="+ limit + apiKey
     print(url1)
     return url1
     #Função que cria e retorna o url da API
@@ -21,24 +22,46 @@ def main():
     #raio=float(input("Quão longe quer viajar em kms: "))
     raio=5000
     raiom=str(raio*1000)
+    distance=0
     #limit=input("Insira qual o número máximo de lugares que quer ver: ")
     limit = 10
     #atrações=input("Insira as suas atrações desejadas separados por virgula, sem espaços: ")
-    atrações = "education,accommodation.hotel,accommodation"
+    atrações = "education,pet,accommodation"
     atraçõesList=atrações.split(",")
-    apil=link(coordenadas,raiom,atrações)
-    #pus isto que sao as cenas q temos que pedir a pessoa, btw aqui em baixo pus tmb a cena com o meu categories pq assim tmb consigo ver isso
+    apil=link(coordenadas,raiom,atrações,limit)
     APIdata=request(apil)
+    aux=0
 
-    #with open(r"C:\Users\Utilizador\Desktop\projeto-FP\Projeto-FP\categories.txt") as file:
+    #with open(r"categories.txt") as file:
         #places = [file.readline()[:-1] for line in file]
     placesk = {}
+   
     for i in range(limit):
+        a=APIdata["features"][i]["properties"]
         for j in range(len(atraçõesList)):
-            if atraçõesList[j] in APIdata["features"][i]["properties"]["categories"]:
-                placesk.setdefault(atraçõesList[j], [])
-                placesk[atraçõesList[j]].append(APIdata["features"][i]["properties"]["name"])
+            if atraçõesList[j] in a["categories"]:
+                try:
+                    placesk.setdefault(atraçõesList[j], [])
+                    placesk[atraçõesList[j]].append(a["name"])
+                    placesk[atraçõesList[j]].append(a["city"])
+                    placesk[atraçõesList[j]].append(a["postcode"])
+                    placesk[atraçõesList[j]].append(a["country"])
+                    placesk[atraçõesList[j]].append(a["street"])
+                    placesk[atraçõesList[j]].append(a["distance"])
+                    placesk[atraçõesList[j]].append(a["lon"])
+                    placesk[atraçõesList[j]].append(a["lat"])
+                    distance = distance + a["distance"]
+                    aux+=1
+                except:
+                    print("bug")
+                try:
+                    placesk[atraçõesList[j]].append(a["datasource"]["raw"]["phone"])  
+                except: 
+                    print("idfk")           
+                    
+    medium_distance = distance / limit            
     print(placesk)
+    print("Distancia média:",medium_distance/1000,"kms")
+    print("número de lugares encontrados:", aux)
     #placesk é um dicionário com as categorias como chave
-
 main()
